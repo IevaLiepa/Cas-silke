4508978c92f9bc9e8d3f185b9ef6b427033927f0
IevaLiepa <167983954+IevaLiepa@users.noreply.github.com>
2024-07-21 10:36:07 +0300

    carUpdate

--- js/Car.js
+++ js/Car.js
@@ -12,7 +12,7 @@ export class Car {
 
     ijungtiVarikli() {
         if (this.isEngineOn === true) {
-            return 'ijungto varyklio dar karta ijungti negalima, sugadinsi starteri .💥';
+            return 'ijungto varyklio dar karta uzkurti neimanoma';
         }
         this.isEngineOn = true;
         return 'Variklis ijungtas';
@@ -20,10 +20,10 @@ export class Car {
 
     isjungtiVarikli() {
         if (this.isEngineOn === false) {
-            return 'isjungto varyklio isjungti negalima...🤔';
+            return 'Nori uzgesinti jau uzgesinta varikli?';
         }
         if (parseFloat(this.speed) !== 0) {
-            return 'Variklis negali buti isjungtas jei automobilis nestovi vietoje!!';
+            return 'Privalai pilnai sustoti jei nori isjungti varikli!';
         }
         this.isEngineOn = false;
         return 'Variklis isjungtas';
@@ -31,22 +31,22 @@ export class Car {
 
     pradetiVaziuoti() {
         if (this.isEngineOn === false) {
-            return 'Negalima pradeti vaziuoti kol variklis neijungtas';
+            return 'Pirmiausia ijunk varikli!';
         }
         if (parseFloat(this.speed) !== 0) {
-            return 'Automobilis jau pajudejes';
+            return 'Automobilis jau pajudejas';
         }
         if (parseFloat(this.fuelLeft) < 2 * parseFloat(this.avgFuelConsumption)) {
-            return 'neuztenka kuro pradeti vaziuoti';
+            return 'Reikia uzsipilti kuro';
         }
         this.speed = '10km/h';
         this.fuelLeft = `${(parseFloat(this.fuelLeft) - (2 * parseFloat(this.avgFuelConsumption))).toFixed(2)}ltr`;
-        return 'Pajudejom is vietos!';
+        return 'Po biski judam';
     };
 
     vaziuoti() {
         if (parseFloat(this.speed) === 0) {
-            return 'Pirmiausia reikia pajudeti is vietos';
+            return 'Pirmiausia reikia ijungti pavara';
         }
         if (parseFloat(this.fuelLeft) < parseFloat(this.avgFuelConsumption)) {
             return 'nebeuztenka kuro';
@@ -57,7 +57,7 @@ export class Car {
 
     sustoti() {
         if (parseFloat(this.speed) === 0) {
-            return 'Automobilis jau stovi vietoje!';
+            return 'Silke jau stovi vietoje!';
         }
         this.speed = '0km/h';
         return 'Automobilis sustojo';
@@ -67,30 +67,3 @@ export class Car {
         return this.fuelLeft;
     };
 
-    uzpiltiKuro(a = 0) {
-        if (parseFloat(this.speed) !== 0) {
-            return 'Negalima uzsipilti kuro vaziuojant';
-        }
-        if (this.isEngineOn === true) {
-            return 'Negalima pilti kuro esant ijungtam varikliui';
-        }
-        const kiekKuroTelpa = `${(parseFloat(this.fuelTankCapacity) - parseFloat(this.fuelLeft)).toFixed(2)}ltr`;
-        if (kiekKuroTelpa === '0.00ltr') {
-            return 'Kuro bakas pilnas';
-        }
-        if (a > parseFloat(kiekKuroTelpa)) {
-            return `Galima uzpilti nedaugiau kaip ${kiekKuroTelpa}`;
-        }
-        if (`${a}` === 'NaN') {
-            return 'Ivestas neatpazintas kuro kiekis';
-        }
-        if (a === 0) {
-            return `Kuro neipilta`;
-        }
-        if (a < 0) {
-            return 'Pavojus!! Kvepia nelegalia veikla!! Si funkcija skirta ipilti kuro, o ne issiurbti!';
-        }
-        this.fuelLeft = `${(a + parseFloat(this.fuelLeft)).toFixed(2)}ltr`;
-        return `Ipilta kuro: ${a.toFixed(2)}ltr`;
-    };
-}
